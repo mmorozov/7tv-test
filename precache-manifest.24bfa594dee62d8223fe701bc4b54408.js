1ccf241215b2d33a01422142cab694d017b5d855
Mikhail Morozov <mihmoz@yandex.ru>
2018-11-17 22:41:09 +0300

    Updates

---
+++ precache-manifest.24bfa594dee62d8223fe701bc4b54408.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e747fd190624f4fbbf8c",
+    "url": "/7tv-test/static/js/0.e747fd19.chunk.js"
+  },
+  {
+    "revision": "78ca97eb39de60d573b2",
+    "url": "/7tv-test/static/css/main.22486c77.chunk.css"
+  },
+  {
+    "revision": "78ca97eb39de60d573b2",
+    "url": "/7tv-test/static/js/main.78ca97eb.chunk.js"
+  },
+  {
+    "revision": "f29bd6a2ccee94990d8b",
+    "url": "/7tv-test/static/js/2.f29bd6a2.chunk.js"
+  },
+  {
+    "revision": "e32f1a1adaf68125a582",
+    "url": "/7tv-test/static/css/3.9bc3f9ff.chunk.css"
+  },
+  {
+    "revision": "e32f1a1adaf68125a582",
+    "url": "/7tv-test/static/js/3.e32f1a1a.chunk.js"
+  },
+  {
+    "revision": "a8dc4046b922078e3243",
+    "url": "/7tv-test/static/js/4.a8dc4046.chunk.js"
+  },
+  {
+    "revision": "1c9c3df2fc06d8225677",
+    "url": "/7tv-test/static/js/runtime~main.1c9c3df2.js"
+  },
+  {
+    "revision": "9096383a78b93f99f783f4efaf367de1",
+    "url": "/7tv-test/index.html"
+  }
+];
